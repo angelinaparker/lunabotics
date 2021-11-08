7a0d27ee03a7035187658b7fb820224bfc0f04e8
Andrew Chung <68363626+andrewchungg@users.noreply.github.com>
2021-11-07 23:19:25 -0800

    Update aStarPrototype2.py

--- Prototypes/Autonomous Path Planning/aStarPrototype2.py
+++ Prototypes/Autonomous Path Planning/aStarPrototype2.py
@@ -1,1 +1,38 @@
 # for visualization of a-star
+
+import pygame
+import math
+from queue import PriorityQueue
+
+WIDTH = 800
+WIN = pygame.display.set_mode((WIDTH, WIDTH))           # set up display to be square
+pygame.display.set_caption("A* Path Finding Algorithm")     # set up caption for window
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 255, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165, 0)
+GREY = (128, 128, 128)
+TURQUOISE = (64, 224, 208)
+
+
+class Node:
+    """Keep track of nodes within the grid"""
+
+    def __int__(self, row, col, width, total_rows):
+        self.row = row
+        self.col = col
+        self.x = row * width
+        self.y = col * width
+        self.color = WHITE
+        self.neighbors = []
+        self.width = width
+
+    def get_pos(self):
+        return self.row, self.col
+
+    def is_closed(self):
