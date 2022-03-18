a1742d52c9c008351a2439c864a167f408a0cdd0
Andrew Chung <68363626+andrewchungg@users.noreply.github.com>
2022-03-17 18:42:05 -0700

    Update guess_aruco_type.py

--- Vision/guess_aruco_type.py
+++ Vision/guess_aruco_type.py
@@ -37,16 +37,51 @@ ARUCO_DICT = {
 print("[INFO] loading image...")
 image = cv2.imread(args["image"])
 image = imutils.resize(image, width=600)
-# loop over the types of ArUco dictionaries
-for (arucoName, arucoDict) in ARUCO_DICT.items():
-	# load the ArUCo dictionary, grab the ArUCo parameters, and
-	# attempt to detect the markers for the current dictionary
-	arucoDict = cv2.aruco.Dictionary_get(arucoDict)
-	arucoParams = cv2.aruco.DetectorParameters_create()
-	(corners, ids, rejected) = cv2.aruco.detectMarkers(
-		image, arucoDict, parameters=arucoParams)
-	# if at least one ArUco marker was detected display the ArUco
-	# name to our terminal
-	if len(corners) > 0:
-		print("[INFO] detected {} markers for '{}'".format(
-			len(corners), arucoName))
+
+# verify that the supplied ArUCo tag exists and is supported by
+# OpenCV
+if ARUCO_DICT.get(args["type"], None) is None:
+	print("[INFO] ArUCo tag of '{}' is not supported".format(
+		args["type"]))
+	sys.exit(0)
+# load the ArUCo dictionary, grab the ArUCo parameters, and detect
+# the markers
+print("[INFO] detecting '{}' tags...".format(args["type"]))
+arucoDict = cv2.aruco.Dictionary_get(ARUCO_DICT[args["type"]])
+arucoParams = cv2.aruco.DetectorParameters_create()
+(corners, ids, rejected) = cv2.aruco.detectMarkers(image, arucoDict,
+	parameters=arucoParams)
+
+# verify *at least* one ArUco marker was detected
+if len(corners) > 0:
+	# flatten the ArUco IDs list
+	ids = ids.flatten()
+	# loop over the detected ArUCo corners
+	for (markerCorner, markerID) in zip(corners, ids):
+		# extract the marker corners (which are always returned in
+		# top-left, top-right, bottom-right, and bottom-left order)
+		corners = markerCorner.reshape((4, 2))
+		(topLeft, topRight, bottomRight, bottomLeft) = corners
+		# convert each of the (x, y)-coordinate pairs to integers
+		topRight = (int(topRight[0]), int(topRight[1]))
+		bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+		bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+		topLeft = (int(topLeft[0]), int(topLeft[1]))
+				# draw the bounding box of the ArUCo detection
+		cv2.line(image, topLeft, topRight, (0, 255, 0), 2)
+		cv2.line(image, topRight, bottomRight, (0, 255, 0), 2)
+		cv2.line(image, bottomRight, bottomLeft, (0, 255, 0), 2)
+		cv2.line(image, bottomLeft, topLeft, (0, 255, 0), 2)
+		# compute and draw the center (x, y)-coordinates of the ArUco
+		# marker
+		cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+		cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+		cv2.circle(image, (cX, cY), 4, (0, 0, 255), -1)
+		# draw the ArUco marker ID on the image
+		cv2.putText(image, str(markerID),
+			(topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX,
+			0.5, (0, 255, 0), 2)
+		print("[INFO] ArUco marker ID: {}".format(markerID))
+		# show the output image
+		cv2.imshow("Image", image)
+		cv2.waitKey(0)
